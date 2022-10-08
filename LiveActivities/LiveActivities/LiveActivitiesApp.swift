f2b1a73c145f56899a33e8ee20068f79c543b2e6
Ignatio Julian <ignatio.tara@astronauts.id>
2022-10-08 08:55:32 +0700

    Initial Project

---
+++ LiveActivities/LiveActivities/LiveActivitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivitiesApp.swift
+//  LiveActivities
+//
+//  Created by Ignatio Julian on 08/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
